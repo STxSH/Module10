4399c9ff4feb16921ef298b07487e15fee15eb76
Anton <anton.batalov94@gmail.com>
2023-10-21 18:39:17 +0700

    task 10.1.4 done

--- Module10/Program.cs
+++ Module10/Program.cs
@@ -2,10 +2,43 @@
 {
     internal class Program
     {
+
+        //task 10.1.4
         static void Main(string[] args)
         {
-           
 
+
+
+        }
+    }
+
+    public interface IManager
+    {
+        void Create();
+        void Read();
+        void Update();
+        void Delete();
+
+    }
+    public class Manager : IManager
+    {
+        public void Create()
+        {
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
 
         }
     }
