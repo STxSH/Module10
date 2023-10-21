3e3aef00ecdfbff561e3fe4b9ed68b066190dce1
Anton <anton.batalov94@gmail.com>
2023-10-21 19:16:51 +0700

    task 10.3.1 done

--- Module10/Program.cs
+++ Module10/Program.cs
@@ -3,27 +3,45 @@
     internal class Program
     {
 
-        //task 10.2.4
+        //task 10.3.1
         static void Main(string[] args)
         {
-            Worker worker = new Worker();
 
-            ((IWorker) worker).Build();
 
             Console.ReadKey();
         }
     }
-    public interface IWorker
+
+    public interface IWriter
     {
-        public void Build();
+        void Write();
     }
 
-    public class Worker :IWorker
+    public interface IReader
     {
-        void IWorker.Build()
-        {
+        void Read();
+    }
 
+    public interface IMailer
+    {
+        void SendEmail();
+    }
+
+    public class FileMAnager : IWriter, IReader, IMailer
+    {
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write()
+        {
+            throw new NotImplementedException();
         }
     }
-
 }
